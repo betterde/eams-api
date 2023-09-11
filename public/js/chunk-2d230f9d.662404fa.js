39d1dd5b454bd0aef7a4c50f366433fef5b898db
George King <george@betterde.com>
2023-09-11 19:44:44 +0800

    minor: add static files

---
+++ public/js/chunk-2d230f9d.662404fa.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d230f9d"],{ef28:function(n,t,e){"use strict";e.r(t);var a=function(){var n=this,t=n.$createElement;n._self._c;return n._m(0)},s=[function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"main-content"},[e("div",{staticClass:"panel"})])}],c={name:"Dashboard",data:function(){return{classes:["animated","fade-in","fast"]}},mounted:function(){}},i=c,u=e("2877"),l=Object(u["a"])(i,a,s,!1,null,null,null);t["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d230f9d.662404fa.js.map
